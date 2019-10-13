c9fbe6a043676c45919c078dde38705dad8469db
Thundros <32390102+Thundros@users.noreply.github.com>
2019-10-13 11:32:27 -0500

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 
-	this.__GAME_WIDTH = (window.innerWidth);
-	this.__GAME_HEIGHT = (window.innerHeight);
-	this.__PHASER_TYPE = (Phaser.AUTO);
-	this.__PHASER_PARENT = ('game');
-	this.__PHASER_SCALE_MODE = (Phaser.Scale.RESIZE);
-	this.__PHASER_SCALE_AUTOCENTER = (Phaser.Scale.CENTER_BOTH);
-	this.__physicsType = ('arcade');
+	this.__GAME_WIDTH = window.innerWidth;
+	this.__GAME_HEIGHT = window.innerHeight;
+	this.__PHASER_TYPE = Phaser.AUTO;
+	this.__PHASER_PARENT = 'game';
+	this.__PHASER_SCALE_MODE = Phaser.Scale.RESIZE;
+	this.__PHASER_SCALE_AUTOCENTER = Phaser.Scale.CENTER_BOTH;
+	this.__physicsType = 'arcade';
 	this.__gravity = 500;
 	this.__movementSpeed = 400;
 	this.__jumpHeight = 550;
